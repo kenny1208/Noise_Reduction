d34bc673d7f85430e68c785d28f72d287dbf3914
kenny1208 <kenny1208@gmail.com>
2025-03-20 11:43:14 +0800

    Salt & Pepper Noise + mean filter

--- noise_reduction.cpp
+++ noise_reduction.cpp
@@ -9,31 +9,71 @@ using namespace std;
 int main() {
 	FILE* fp_r;
 	FILE* fp_w;
+	FILE* fp_h;
 
 	bool err_r = fopen_s(&fp_r, "AiPicG.bmp", "rb");
 	bool err_w = fopen_s(&fp_w, "AIgened_x.bmp", "wb");
+	bool err_h = fopen_s(&fp_h, "AIgened_y.bmp", "wb");
 	if (err_r != 0 || err_w != 0)
 		return 0;
 
 	unsigned char c;
-	for (int i = 0; i < 1078; i++)
+
+	for (int i = 0; i < 1078; i++) 
 	{
 		c = getc(fp_r);
 		putc(c, fp_w);
+		putc(c, fp_h);
 	}
+
 
 	int w = 1024;
 	int h = 1024;
 
-	
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> noise_prob(1, 20);
+	uniform_int_distribution<> black_or_white(0, 1);
+
 	vector<vector<unsigned char>> imgArr;
 	imgArr.resize(h, vector<unsigned char>(w));
+
+	for (int i = 0; i < w * h; i++)
+	{
+		int x = i % w;
+		int y = i / w;
+		int pixel = getc(fp_r);
+
+		if (noise_prob(gen) == 1)
+		{
+			pixel = (black_or_white(gen) == 0) ? 0 : 255;
+		}
+
+		imgArr[x][h - y - 1] = static_cast<unsigned char>(pixel);
+	}
+
+	for (int i = h - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			putc(imgArr[j][i], fp_w);
+		}
+
+	}
+
+	fclose(fp_r);
+	fclose(fp_w);
+
+	err_w = fopen_s(&fp_w, "AIgened_x.bmp", "rb");
+
+	vector<vector<unsigned char>> imgArr2;
+	imgArr2.resize(h, vector<unsigned char>(w));
 
 	for (int i = h - 1; i >= 0; i--) 
 	{  
 		for (int j = 0; j < w; j++) 
 		{
-			imgArr[i][j] = getc(fp_r);
+			imgArr2[i][j] = getc(fp_w);
 		}
 	}
 
@@ -42,7 +82,8 @@ int main() {
 	{
 		for (int j = 0; j < w; j++)
 		{
-			int sum = 0, count = 0;
+			int sum[9] = {};
+			int count = 0;
 
 			int dx[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
 			int dy[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
@@ -54,12 +95,12 @@ int main() {
 
 				if (ni >= 0 && ni < h && nj >= 0 && nj < w) 
 				{
-					sum += imgArr[ni][nj];
+					sum[k] = imgArr[ni][nj];
 					count++;
 				}
 			}
-
-			imgArr[i][j] = (count > 0) ? (sum / count) : imgArr[i][j];
+			sort(sum, sum + 9);	
+			imgArr2[j][i] = sum[4];
 		}
 	}
 
@@ -68,7 +109,7 @@ int main() {
 	{
 		for (int j = 0;j < w;j++)
 		{
-			putc(imgArr[i][j], fp_w);
+			putc(imgArr2[i][j], fp_h);
 		}
 	}
 
